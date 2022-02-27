16c76a952378689d3862b46d41ccf08237820ea6
Travis Bee <99930434+tbee10@users.noreply.github.com>
2022-02-27 12:20:18 -0700

    Add files via upload

---
+++ password.js
@@ -0,0 +1,15 @@
+//I know I didn't do this totally correctly. I know this would really need HTML and CSS as well. But I just tried to code what would be the JS code portion, which is probably still not correct.
+
+var welcome = ('Welcome to the password validator tool')
+var enter = ('Please enter a valid password')
+console.log(welcome);
+console.log(enter);
+
+let password = ()
+if (password.length >= 10){
+    console.log('Password Successful')
+}
+else {
+    console.log('Password Invalid')
+}
+
