b79ba95ed57aae0ed517758f407124b360e661be
Travis Bee <99930434+tbee10@users.noreply.github.com>
2022-03-08 19:16:23 -0700

    Add files via upload

--- password.js
+++ password.js
@@ -1,15 +1,23 @@
-//I know I didn't do this totally correctly. I know this would really need HTML and CSS as well. But I just tried to code what would be the JS code portion, which is probably still not correct.
+const readLine = require('readline')
 
-var welcome = ('Welcome to the password validator tool')
-var enter = ('Please enter a valid password')
-console.log(welcome);
-console.log(enter);
+const reader = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
-let password = ()
-if (password.length >= 10){
-    console.log('Password Successful')
-}
-else {
-    console.log('Password Invalid')
-}
+reader.question(
+    'Welcome to the password validator! Please input a password: ',
+    function (input) {
+        console.log(input)
+        console.log(input.length)
+
+        if (input.length >= 10){
+            console.log('Your password is successful!')
+         } else {
+                console.log('Your password is invalid')
+               
+                reader.close()
+            }
+        }
+)
 
